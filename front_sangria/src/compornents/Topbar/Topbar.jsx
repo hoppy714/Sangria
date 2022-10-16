93a8521a86ff4e2f3f4a4cb36f52b916221033bc
hoppy714 <hodaka_itou@me.com>
2022-10-16 22:38:59 +0900

    Topbar complete

--- front_sangria/src/compornents/Topbar/Topbar.jsx
+++ front_sangria/src/compornents/Topbar/Topbar.jsx
@@ -1,19 +1,31 @@
+import { Chat, Notifications, Search } from '@mui/icons-material'
 import React from 'react'
+import "./Topbar.css"
 
 export default function Topbar() {
   return (
-    <div className='topberCompornent'>
+    <div className='topberContainer'>
       <div className='topbarLeft'>
         <span className='logo'>PachinkoBIG</span>
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
+          <Search className='searchIcon'/>
           <input type="text" className="searchInput" placeholder='見つけたいものを検索しよう' />
         </div>
       </div>
       <div className="topbarRight">
-        <div className="topbarIconItem">1</div>
-        <div className="topbarIconItem">2</div>
+        <div className="topbarIconItems">
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">2</span>
+          </div> 
+          <img src="/assets/profile.png" alt="" className="topbarImg" />
+        </div>
       </div>
     </div>
   )
