6b82b33c007cc85e86782de9d8984f137024d65a
hoppy714 <hodaka_itou@me.com>
2022-10-18 14:21:27 +0900

    kowaikara commit

--- front_sangria/src/compornents/post/Post.jsx
+++ front_sangria/src/compornents/post/Post.jsx
@@ -1,8 +1,35 @@
+import { MoreVert } from '@mui/icons-material'
 import React from 'react'
 import './Post.css'
 
 export default function Post() {
   return (
-    <div>Post</div>
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img src="./assets/person/1.png" alt="" className="postProfileImg" />
+            <span className="postUsername">hoppin</span>
+            <span className="postDate">3分前</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">SNSdayon</span>
+          <img src="./assets/post.jpg" alt="" className="postImg" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img src="./assets/heart.jpg" alt="" className="likeIcon" />
+            <span className="postLikeCounter">5人がいいねを押しました</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postComment">コメント:4件</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- front_sangria/src/compornents/rightbar/Rightbar.jsx
+++ front_sangria/src/compornents/rightbar/Rightbar.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import './Rightbar.css'
 
 export default function Rightbar() {
   return (
-    <div>Rightbar</div>
+    <div className='rightbar'>
+      <div className="rightbarWrapper">
+        <div className="eventContainer">
+          <img src="" alt="" className="starImg" />
+          <span className="eventText"><b>本日イベント日！</b>激熱です！</span>
+        </div>
+        <img src="assets/event.jpg" alt="" />
+        <h4 className="rightbarTitle">遊戯中の友達</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src="" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="username"></span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
