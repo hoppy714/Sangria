3a108076137f391861047eeb7e80b4fa01f758b7
hoppy714 <hodaka_itou@me.com>
2022-11-22 12:02:03 +0900

    css変更

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-// import Login from "./pages/login/Login0";
+
 import Login from "./pages/login/Login";
-// import Home from "./pages/home/Home";
-// import Profile from "./pages/profile/Profile";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import { BrowserRouter as router, Router, Routes } from "react-router-dom"
+
 
 function App() {
-  return <Login />;
+  return <Home />;
 }
 
 export default App;
