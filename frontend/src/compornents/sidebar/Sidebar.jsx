c96d5443bad513bb8782f6e3d7e94da9378c5995
hoppy714 <hodaka_itou@me.com>
2022-11-25 09:49:01 +0900

    settings html

--- frontend/src/compornents/sidebar/Sidebar.jsx
+++ frontend/src/compornents/sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Description, Home, MessageRounded, Notifications, Person, Search, Settings } from "@mui/icons-material";
 import "./Sidebar.css"
+import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
   return (
@@ -8,50 +9,56 @@ export default function Sidebar() {
       <div className="sidebarWrapper">
         <ul className="sidebarlist">
           <li className="sidebarListItem">
-            <Home className="sidebarIcon"/>
-            <span className="sidebarListItemText">ホーム</span>
+            <Home className="sidebarIcon" />
+            <Link to="/" style={{ color: "#5e5e5e", textDecoration: "none" }}>
+              <span className="sidebarListItemText">ホーム</span>
+            </Link>
           </li>
           <li className="sidebarListItem">
-            <Search className="sidebarIcon"/>
+            <Search className="sidebarIcon" />
             <span className="sidebarListItemText">検索</span>
           </li>
           <li className="sidebarListItem">
-            <Notifications className="sidebarIcon"/>
+            <Notifications className="sidebarIcon" />
             <span className="sidebarListItemText">通知</span>
           </li>
           <li className="sidebarListItem">
-            <MessageRounded className="sidebarIcon"/>
+            <MessageRounded className="sidebarIcon" />
             <span className="sidebarListItemText">DM</span>
           </li>
           <li className="sidebarListItem">
-            <Description className="sidebarIcon"/>
+            <Description className="sidebarIcon" />
             <span className="sidebarListItemText">収支表</span>
           </li>
           <li className="sidebarListItem">
-            <Person className="sidebarIcon"/>
-            <span className="sidebarListItemText">プロフィール</span>
+            <Person className="sidebarIcon" />
+            <Link to="/profile/hoppy" style={{ color: "#5e5e5e", textDecoration: "none" }}>
+              <span className="sidebarListItemText">プロフィール</span>
+            </Link>
           </li>
           <li className="sidebarListItem">
-            <Settings className="sidebarIcon"/>
-            <span className="sidebarListItemText">設定</span>
+            <Settings className="sidebarIcon" />
+            <Link to="/settings" style={{ color: "#5e5e5e", textDecoration: "none" }}>
+              <span className="sidebarListItemText">設定</span>
+            </Link>
           </li>
         </ul>
         <hr className="sidebarHr" />
         <ul className="sidebarFriendList">
           <li className="sidebarFriend">
-            <img src="/assets/person/2.jpg" alt="" className='sidebarFriendImg'/>
+            <img src="/assets/person/2.jpg" alt="" className='sidebarFriendImg' />
             <span className="sidebarFriendName">Ginjiro Abe</span>
           </li>
           <li className="sidebarFriend">
-            <img src="/assets/person/3.jpg" alt="" className='sidebarFriendImg'/>
+            <img src="/assets/person/3.jpg" alt="" className='sidebarFriendImg' />
             <span className="sidebarFriendName">Ken Hatakenaka</span>
           </li>
           <li className="sidebarFriend">
-            <img src="/assets/person/4.jpg" alt="" className='sidebarFriendImg'/>
+            <img src="/assets/person/4.jpg" alt="" className='sidebarFriendImg' />
             <span className="sidebarFriendName">Takayuki Tamai</span>
           </li>
         </ul>
       </div>
-    </div>
+    </div >
   )
 }
--- frontend/src/pages/Settings/Settings.jsx
+++ frontend/src/pages/Settings/Settings.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import Topbar from '../../compornents/Topbar/Topbar';
 import Sidebar from '../../compornents/sidebar/Sidebar';
-import './settings.css'
-export default function test() {
+// import './settings.css'
+export default function Settings() {
     return (
         <>
             <Topbar />
             <div className="setting">
-            <Sidebar />
+                <Sidebar />
                 <div className="settingWrapper">
-                    <ul className="settingList" style="list-style: none;">
+                    <ul className="settingList">
                         <li className="settingListItem"></li>
                         <details>
                             <summary><strong>アカウント管理</strong></summary>
                             {/*ここから先を折りたたむ*/}
-                            <ul className="settingList" style="list-style: none;">
+                            <ul className="settingList">
                                 <li className="settingListClick">
                                     <div className="settingListClickItem">アカウント情報</div>
                                 </li>
@@ -29,7 +29,7 @@ export default function test() {
                         <details>
                             <summary><strong>セキュリティとアカウントアクセス</strong></summary>
                             {/*ここから先を折りたたむ*/}
-                            <ul className="settingList" style="list-style: none;">
+                            <ul className="settingList" >
                                 <li className="settingListClick">
                                     <div className="settingListClickItem">セキュリティ</div>
                                 </li>
@@ -43,7 +43,7 @@ export default function test() {
                         <details>
                             <summary><strong>プライバシーと安全</strong></summary>
                             {/*ここから先を折りたたむ*/}
-                            <ul className="settingList" style="list-style: none;">
+                            <ul className="settingList">
                                 <li className="settingListClick">
                                     <div className="settingListClickItem">ツイート</div>
                                 </li>
@@ -54,7 +54,7 @@ export default function test() {
                         <details>
                             <summary><strong>通知</strong></summary>
                             {/*ここから先を折りたたむ*/}
-                            <ul className="settingList" style="list-style: none;">
+                            <ul className="settingList" >
                                 <li className="settingListClick">
                                     <div className="settingListClickItem">通知を受け取る</div>
                                 </li>
@@ -65,7 +65,7 @@ export default function test() {
                         <details>
                             <summary><strong>アクセシビリティ、表示</strong></summary>
                             {/*ここから先を折りたたむ*/}
-                            <ul className="settingList" style="list-style: none;">
+                            <ul className="settingList" >
                                 <li className="settingListClick">
                                     <div className="settingListClickItem">アクセシビリティ</div>
                                 </li>
